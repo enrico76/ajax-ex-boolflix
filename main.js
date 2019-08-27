29565cbfe8059a1f3238327df5d071cf544fc63d
enrico76 <enricodip@gmail.com>
2019-08-27 21:20:04 +0200

    function to store the entered value

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("initial script");
+console.log("ciao");
+
+// API key
+var APIKEY = "4ecca77150719dcc4afd1c3dc1215ed8";
+// indirizzo di ricerca
+var APIURL = "https://api.themoviedb.org/3";
+
+function searchClick() {
+  console.log("sono qui");
+  var query = $('#titolo_digit').val();
+  console.log(query);
+
+}
+function initial() {
+  // console.log("init");
+  $("#myButton").click(searchClick);
+
+}
+$(document).ready(searchClick);
